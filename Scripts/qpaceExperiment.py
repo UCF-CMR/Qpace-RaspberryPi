7f40c2204de391206e645c88bcb033602bccac36
bobacrazy <minh.pham@knights.ucf.edu>
2018-05-15 15:39:51 -0400

    Added experiment parser and edited experiment module.

--- Scripts/qpaceExperiment.py
+++ Scripts/qpaceExperiment.py
@@ -7,208 +7,124 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setwarning(False)
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
 def pinInit():
-	"""
-	This function initializes all of the pins for the experiment.
-
-	Parameters
-	----------
-	None.
-
-	Returns
-	-------
-	None.
-
-	"""
-
 	#GoPro pin setup
-	GPIO.setup(19, GPIO.OUT, initial=0)				#Power
-	GPIO.setup(13, GPIO.OUT, initial=1)				#On Button
-	GPIO.setup(15, GPIO.OUT, initial=1)				#Capture Button
+	GPIO.setup(19, GPIO.OUT, initial=0)			#Power
+	GPIO.setup(13, GPIO.OUT, initial=1)			#On Button
+	GPIO.setup(15, GPIO.OUT, initial=1)			#Capture Button
 
 	#Stepper pin setup
-	GPIO.setup(33, GPIO.OUT, initial=0)				#Step Enable
-	GPIO.setup(29, GPIO.OUT, initial=0)				#Step A Enable
-	GPIO.setup(21, GPIO.OUT, initial=0)				#Step B Enable
+	GPIO.setup(33, GPIO.OUT, initial=0)			#Step Enable
+	GPIO.setup(29, GPIO.OUT, initial=0)			#Step A Enable
+	GPIO.setup(21, GPIO.OUT, initial=0)			#Step B Enable
 
 	#LED pin setup
-	GPIO.setup(23, GPIO.OUT)						#Controls the LEDs
-    
+	GPIO.setup(23, GPIO.OUT)					#Controls the LEDs
+
     #Solenoid setup
-    GPIO.setup(35, GPIO.OUT)						#Solenoid 1
-    GPIO.setup(31, GPIO.OUT)						#Solenoid 2
-    GPIO.setup(37, GPIO.OUT)						#Solenoid 3
+	GPIO.setup(35, GPIO.OUT, initial=1)					#Solenoid 1
+	GPIO.setup(31, GPIO.OUT, initial=1)					#Solenoid 2
+	GPIO.setup(37, GPIO.OUT, initial=1)					#Solenoid 3
 
 
 def goPro(recordingTime):
-    """
-    This function activates the GoPro camera.
-
-    Parameters
-    ----------
-    Integer / Float - recordingTime - The desired time that the camera will be 
-    	recording for, in seconds.
-
-    Returns
-    -------
-    None.
-	
-    """
 	#Turning on the device
 	GPIO.output(19, 1)
-	sleep(3)
+	time.sleep(3)
 	GPIO.output(13, 0)
-	sleep(1)
+	time.sleep(1)
 	GPIO.output(13, 1)
-	sleep(10)
+	time.sleep(10)
 
 	#Begin Recording
 	GPIO.output(15, 0)
-	sleep(0.5)
+	time.sleep(0.5)
 	GPIO.output(15, 1)
 
 	#Recording time
-	sleep(recordingTime)
+	time.sleep(recordingTime)
 
 	#Stop Recording
 	GPIO.output(15, 0)
-	sleep(0.5)
+	time.sleep(0.5)
 	GPIO.output(15, 1)
 
 	#Call Subprocess "hc-star" to enable USB hub
-	print("Transfering Data...")
+	#print("Transfering Data...")
 	#subprocess.call(["/home/pi/hc-start"])
 
 	#Shutdown Device
 	GPIO.output(13, 0)
-	sleep(5)
-	GPIO.outpit(13, 1)
+	time.sleep(5)
+	GPIO.output(13, 1)
 
 	GPIO.output(19, 0)
-    
+
 
 def stepper(delay, qturn):
-	"""
-	This function activates the stepper motor.
+    #Setstep definition
+    def setStep(a, b):
+        GPIO.output(29, a)
+        GPIO.output(21, b)
 
-	Parameters
-	----------
-	Integer / Float - delay - The time between each phase of the stepper motor.
-	Integer - qturn - The number of turns.
+	GPIO.output(33, True)
 
-	Returns
-	-------
-	None.
+    #qturn
+    for i in range(0, qturn):
+        setStep(1, 1)
+        time.sleep(delay)
+        setStep(0, 1)
+        time.sleep(delay)
+        setStep(0, 0)
+        time.sleep(delay)
+        setStep(1, 0)
+        time.sleep(delay)
 
-	"""
-	#Setstep definition
-	
-	def setStep(a, b):
-		GPIO.output(29, a)
-		GPIO.output(21, b)
-    
-	#qturn
-	'''
-	for i in range(0, qturn):
-		setStep(1, 0, 1, 0)
-		time.sleep(delay)
-		setStep(0, 1, 1, 0)
-		time.sleep(delay)
-		setStep(0, 1, 0, 1)
-		time.sleep(delay)
-		setStep(1, 0, 0, 1)
-		time.sleep(delay)
-		setStep(1, 0, 1, 0)
-		time.sleep(delay)
-	
-	'''
+    time.sleep(3)
 
+    #reverse qturn
+    for i in range(0, qturn):
+        setStep(1, 1)
+        time.sleep(delay)
+        setStep(1, 0)
+        time.sleep(delay)
+        setStep(0, 0)
+        time.sleep(delay)
+        setStep(0, 1)
+        time.sleep(delay)
 
-	for i in range(0, qturn):
-		setStep(1, 1)
-		time.sleep(delay)
-		setStep(0, 1)
-		time.sleep(delay)
-		setStep(0, 0)
-		time.sleep(delay)
-		setStep(1, 0)
-		time.sleep(delay)
+	GPIO.output(33, False)
+    #complete
 
-	time.sleep(3)
-
-	#reverse qturn
-	'''
-	for i in range(0, qturn):
-		setStep(1, 0, 1, 0)
-		time.sleep(delay)
-		setStep(1, 0, 0, 1)
-		time.sleep(delay)
-		setStep(0, 1, 0, 1)
-		time.sleep(delay)
-		setStep(0, 1, 1, 0)
-		time.sleep(delay)
-		setStep(1, 0, 1, 0)
-		time.sleep(delay)
-	'''
-
-	for i in range(0, qturn):
-		setStep(1, 1)
-		time.sleep(delay)
-		setStep(1, 0)
-		time.sleep(delay)
-		setStep(0, 0)
-		time.sleep(delay)
-		setStep(0, 1)
-		time.sleep(delay)
-
-	#complete
-    
 def led(power):
-	"""
-	This function turns the LED light on or off.
-
-	Parameters
-	----------
-	Boolean - power - To turn the LED on/off, power is set to True/False, respectively.
-	"""
 	GPIO.output(23, power)
 
-def solenoid(solPins : list):
-    """
-    This function handles the operation of the solenoids.
+def solenoid(solPins : list, duration):
+"""
+solPins is a list of tuples, each containing a frequency (in Hz) and a solenoid
+pin number (<freq, pinNum>).
 
-    Parameters
-    ----------
-	List of Tuples - solPins - List that holds tuples of the form (<duty cycle>, <pin number>). 
-		Note: The duty cycle is listed first so that solPins.sort() will sort the pairs by their duty cycles.
+duration is the total time, in seconds, for which the solenoids will be firing.
+"""
+	solPins.sort()
+	counter = 0
 
-	Returns
-	-------
-	None.
+	def fire(pin : tuple):
+		if (pin[0] > 0):
+			dutyCycle = 1 / pin[0]
+			GPIO.output(pin[1], False)		#Turns solenoid on
+			time.sleep(dutyCycle / 2)		#Waits for half of the duty cycle
+			GPIO.output(pin[1], True)		#Turns solenoid off
+			time.sleep(dutyCycle / 2)
+			counter += dutyCycle			#increments counter
 
-    """
-    solPins.sort()
-    # Set all solenoids off by default
-    GPIO.output(35, True)
-    GPIO.output(31, True)
-    GPIO.output(37, True)
+	#counter maintains how long the solenoids have been firing.
+	while (counter < duration):
+		for i in range(0, len(solPins)):
+			fire(solPins[i])
 
-    def fire(pin : tuple):
-        GPIO.output(pin[1], False)		#Turn solenoid on
-       	time.sleep(pin[0])				#Waits for specific duty cycle
-        GPIO.output(pin[1], True)		#Turn solenoid off
-
-    for i in range(0, 10):
-		fire(solPins[-1])
-		for j in range(0, len(solPins) - 1):
-	    	fire(solPins[j])
-
-
-
-
-
-
-
+def solenoid(freq, duration):
+	counter = 0
---
+++ Scripts/qpaceExperimentParser.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+# qpaceExperimentParser.py by Minh Pham
+#3-06-2018, Rev. 1.1
+#Q-Pace project, Center for Microgravity Research
+#University of Central Florida
+
+import qpaceExperiment
+import qpaceLogger
+
+def experimentparser(filepath):
+    """
+    This function handles the parsing and execution of the raw text experiment files.
+
+    Parameters
+    ----------
+    String - filepath - The path of the experiment file to be parsed.
+
+    Returns
+    -------
+    None.
+
+    Raises
+    ------
+    IOError - Function handles file I/O errors.
+
+    """
+    try:
+        with open(filepath, 'r') as inputFile:
+            for line in inputFile:
+                instruction = line.split()
+                if (instruction[0].upper() == "GOPRO"):
+                    recordingTime = int(instruction[1])
+                    Experiment.goPro(recordingTime)
+
+                elif (instruction[0].upper() == "LED"):
+                    power = (instruction[1].upper() == "TRUE")
+                    Experiment.led(power)
+
+                elif (instruction[0].upper() == "STEPPER"):
+                    delay = int(instruction[1])
+                    qturn = int(instruction[2])
+                    Experiment.stepper(delay, qturn)
+
+                elif (instruction[0].upper() == "SOLENOID"):
+                    solPins = list()
+                    duration = float(instruction[1])
+                    for i in range(2, len(instruction), 2):
+                        solPins.append(tuple((float(instruction[i]), int(instruction[i+1]))))
+                    Experiment.solenoid(solPins)
+
+    # Output to the logger here.
+    except IOError as e:
+        qpaceLogger.logError("Could not open experiment file at " + str(filepath) + ".", e)
+        qpaceLogger.logSystem([["File I/O error occured."]])
