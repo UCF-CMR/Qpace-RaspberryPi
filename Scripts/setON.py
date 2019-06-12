ae622618b397f79c2e6465c4b1dd39c805362e34
Connor Westcott <cww904@gmail.com>
2019-06-12 17:40:13 -0400

    Testing Scripts for Terminal Serial

--- Scripts/setON.py
+++ Scripts/setON.py
@@ -1,13 +1,14 @@
 import os
 import pigpio
+from subprocess import check_output
 
 SCRIPT = "terminalFlood.py"
-GPIO21 = 29 # GPIO 21 is Pin 29
+GPIO21 = 21 # GPIO 21 is Pin 29
 pi = pigpio.pi()
 
 while True:
-    curProcesses = os.system("ps -fA | grep python")
-    if SCRIPT in curProcesses:
+    #curProcesses = os.system("ps -fA | grep python")
+    if SCRIPT in str(check_output("ps -fA | grep python", shell=True)):
         pi.write(GPIO21, 1) # If the script is running set the GPIO 21 to high
     else:
         pi.write(GPIO21, 0)
--- Scripts/terminalFlood.py
+++ Scripts/terminalFlood.py
@@ -1,8 +1,41 @@
 # Dumps 128 Bytes of data to the terminal and nothing else
+import pigpio
+import time
+import serial
+import sys
 
-NUM_PACKETS = 128
-DELAY = 1 #sec
+#if len(sys.argv) < 3:
+#  exit()
 
+isSerial = False
+
+try:
+    serialData = serial.Serial('/dev/ttyAMA0', 115200)
+    isSerial = True
+except:
+    pass
+pi = pigpio.pi()
+
+
+NUM_PACKETS = 1000 #int(sys.argv[1])
+DELAY = 0.1 #float(sys.argv[2])
+
+pi.write(20, 1)
+
+while pi.read(20) != 1:
+    print(pi.read(20))
+
+def printData (data):
+
+    if isSerial:
+        serialData.write(data.encode('ASCII'))
+    else:
+        print(data)
+        
+time.sleep(1)
 for packet in range(NUM_PACKETS):
-    data = str(packet) + "A"*(128-packet)
-    print(data)
+    data = str(packet) + "A"*(128-len(str(packet)))
+    printData(data)
+    #serialData.write(data.encode('ASCII'))
+    time.sleep(DELAY)
+
