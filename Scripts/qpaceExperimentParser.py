20de00d500c0035a188880e58ef833bd39eaf434
Connor Westcott <cww904@gmail.com>
2019-07-26 09:35:48 -0400

    Folder Creation Fixes
    Forgot to grab the last '/' char to make the path an actual directory

--- Scripts/qpaceExperimentParser.py
+++ Scripts/qpaceExperimentParser.py
@@ -283,7 +283,7 @@ def run(filename, isRunningEvent, runEvent,logger,nextQueue,disableCallback):
 									try:
 										recFile = instruction[3]
 										if '/' in recFile:
-											directory = '/home/pi/data/vid/' + recFile[:recFile.rindex('/')]
+											directory = '/home/pi/data/vid/' + recFile[:recFile.rindex('/')+1] # make sure to get the '/'
 											print(directory)
 											if not os.pat.isdir(directory):
 												os.mkdir(directory)
