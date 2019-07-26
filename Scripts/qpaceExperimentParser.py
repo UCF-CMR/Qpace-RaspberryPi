bf100ec9e1b72b51a8189e602ea7647ce678afff
Connor Westcott <cww904@gmail.com>
2019-07-26 09:47:59 -0400

    import errors?
    VS code was saying os was not imported for making the directory

--- Scripts/qpaceExperimentParser.py
+++ Scripts/qpaceExperimentParser.py
@@ -285,6 +285,7 @@ def run(filename, isRunningEvent, runEvent,logger,nextQueue,disableCallback):
 										if '/' in recFile:
 											directory = '/home/pi/data/vid/' + recFile[:recFile.rindex('/')+1] # make sure to get the '/'
 											print(directory)
+											import os
 											if not os.path.isdir(directory):
 												try:
 													os.mkdir(directory)
